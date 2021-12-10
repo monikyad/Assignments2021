e9991978213c8c24ece0eb19dfb30f47ca4bfc1b
monikayad <monikayadav25@gmail.com>
2021-12-09 21:50:49 -0600

    removed unused code

--- src/fthtest/FileReadingTest.java
+++ src/fthtest/FileReadingTest.java
@@ -9,42 +9,42 @@ import java.util.Properties;
 import java.util.Set;
 
 public class FileReadingTest {
-public static void main(String[] args) throws IOException {
-    FileReadingTest fr = new FileReadingTest();
-   fr.doesFileExist(null);  
+    public static void main(String[] args) throws IOException {
+	FileReadingTest fr = new FileReadingTest();
+	fr.doesFileExist(null);
 
-   Properties prop = fr.readFile("src\\fthtest\\data.properties");
-   Set<Object> keys=prop.keySet();
-   for(Object key:keys) {
-     String values= (String)prop.get(key);
-     String[] valArrays= values.split(",");
-     System.out.println(key+": ");
-     for(String val:valArrays) {
-         System.out.println(val);
-     }
-   }
+	Properties prop = fr.readFile("src\\fthtest\\data.properties");
+	// fetch all keys from properties files
+	Set<Object> keys = prop.keySet();
+	for (Object key : keys) {
+	    // downcast to String object
+	    String values = (String) prop.get(key);
+	    String[] valArrays = values.split(",");
+	    System.out.println(key + ": ");
+	    for (String val : valArrays) {
+		System.out.println(val);
+	    }
+	}
 
- 
-   System.out.println("Take: "+ prop.getProperty("Take"));
-}
+    }
 
-public void doesFileExist(String path) {
-   
+    public void doesFileExist(String path) {
+
 	File f = new File("src\\fthtest\\data.properties");
 
-       if (f.exists()) {
-        System.out.println("Exists");
-       }else System.out.println("Does not Exists");
- 
-   
-}
+	if (f.exists()) {
+	    System.out.println("Exists");
+	} else
+	    System.out.println("Does not Exists");
 
-public Properties readFile(String filePath) throws IOException {
-    File f = new File(filePath);
-    FileInputStream fileInput = new FileInputStream(f);
-   
-    Properties prop = new Properties();
-    prop.load(fileInput);
-    return prop;
-}
+    }
+
+    public Properties readFile(String filePath) throws IOException {
+	File f = new File(filePath);
+	FileInputStream fileInput = new FileInputStream(f);
+
+	Properties prop = new Properties();
+	prop.load(fileInput);
+	return prop;
+    }
 }
